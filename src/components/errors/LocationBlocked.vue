f27f326e4dea2f0958f3fa506b0a4358578c43e3
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-05-24 10:44:30 -0400

    added notfound and locationblocked components

---
+++ src/components/errors/LocationBlocked.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section has-background-white">
+    <div class="container">
+      <h1 class="title">We can't find you!</h1>
+      <h2 class="subtitle">
+        Please allow us to see your location by 
+        <a href="https://www.google.com/search?q=enable+browser+location+services" target="_blank">enabling browser location services</a>.
+      </h2>
+      <br>
+      <p class="has-text-centered">
+        <img src="/static/img/settings.svg" style="max-width: 400px;"/>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({ name: 'LocationBlocked' })
+export default class LocationBlocked extends Vue {}
+</script>
---
+++ src/components/errors/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section has-background-white">
+    <div class="container">
+      <h1 class="title">Page not found.</h1>
+      <h2 class="subtitle">
+        <router-link :to="{ path: '/' }"> 
+          home 
+        </router-link>
+      </h2>
+      <br>
+      <p class="has-text-centered">
+        <img src="/static/img/notfound.svg" style="max-width: 800px;"/>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({ name: 'NotFound' })
+export default class NotFound extends Vue {}
+</script>
