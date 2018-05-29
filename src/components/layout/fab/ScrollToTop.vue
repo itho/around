ace8a8359ce50c3c82a58c79095a5eb981fea7b8
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-05-29 15:53:03 -0400

    updated fab

--- src/components/layout/fab/ScrollToTop.vue
+++ src/components/layout/fab/ScrollToTop.vue
@@ -1,21 +1,86 @@
 <template>
-  <button
-    class="mdc-fab ripple app-fab--absolute"
-    aria-label="Add"
-    @click="handleClicked()">
-    <span class="mdc-fab__icon">+</span>
-  </button>
+  <transition name="fab-transition" mode="out-in">
+    <button
+      v-if="scrollTop > 200"
+      class="mdc-fab ripple app-fab--absolute"
+      aria-label="Scroll To Top"
+      @click="handleClicked()">
+      <span class="mdc-fab__icon">
+        <font-awesome-icon icon="chevron-up" size="2x" class="has-text-white"/>
+      </span>
+    </button>
+  </transition>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 
 @Component({
-  name: 'FloatingActionButton'
+  name: 'ScrollToTop',
+  components: { FontAwesomeIcon }
 })
-export default class FloatingActionButton extends Vue {
+export default class ScrollToTop extends Vue {
+  scrollTop: number = 0
+
+  mounted () {
+    document.addEventListener('scroll', (e) => { this.scrollTop = window.scrollY }, true)
+  }
+
   handleClicked () {
-    this.$router.push({ name: 'new-event' })
+    this.scrollIt(0)
+  }
+
+  scrollIt (destination, duration = 300, easing = 'easeInOutCubic', callback = null) {
+    const easings = {
+      linear (t) { return t },
+      easeInQuad (t) { return t * t },
+      easeOutQuad (t) { return t * (2 - t) },
+      easeInOutQuad (t) { return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t },
+      easeInCubic (t) { return t * t * t },
+      easeOutCubic (t) { return (--t) * t * t + 1 },
+      easeInOutCubic (t) { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1 },
+      easeInQuart (t) { return t * t * t * t },
+      easeOutQuart (t) { return 1 - (--t) * t * t * t },
+      easeInOutQuart (t) { return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t },
+      easeInQuint (t) { return t * t * t * t * t },
+      easeOutQuint (t) { return 1 + (--t) * t * t * t * t },
+      easeInOutQuint (t) { return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t }
+    }
+
+    const start = window.pageYOffset;
+    const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+    const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+    const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+    const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+    if ('requestAnimationFrame' in window === false) {
+      window.scroll(0, destinationOffsetToScroll);
+      if (callback) {
+        callback();
+      }
+      return;
+    }
+
+    function scroll() {
+      const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+      const time = Math.min(1, ((now - startTime) / duration));
+      const timeFunction = easings[easing](time);
+      window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+      if (window.pageYOffset === destinationOffsetToScroll) {
+        if (callback) {
+          callback();
+        }
+        return;
+      }
+
+      requestAnimationFrame(scroll);
+    }
+
+    scroll();
   }
 }
 </script>
@@ -63,6 +128,7 @@ export default class FloatingActionButton extends Vue {
     // background-color: #018786;
     color: #fff;
     color: var(--mdc-theme-on-secondary,#fff);
+    z-index: 99;
     background: #4568dc;  /* fallback for old browsers */
     // background: -webkit-linear-gradient(to right, #b06ab3, #4568dc);  /* Chrome 10-25, Safari 5.1-6 */
     // background: linear-gradient(to right, #b06ab3, #4568dc); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
@@ -99,7 +165,7 @@ export default class FloatingActionButton extends Vue {
   .mdc-fab__icon {
     width: 24px;
     height: 24px;
-    font-size: 35px;
-    line-height: 18px;
+    font-size: 12px;
+    line-height: 12px;
   }
 </style>
