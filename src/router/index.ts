8eeb8c49c0b5f076a5f34b9c24a228de7d97b8dd
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-05-29 15:49:28 -0400

    renamed views and components

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,19 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Guard from '../services/middleware'
 
-import HomeComponent from '@/views/home/index.vue'
-import DashboardComponent from '@/views/dashboard/index.vue'
-import EventsComponent from '@/views/events/index.vue'
-import NewEventComponent from '@/views/events/new.vue'
-import ShowEventComponent from '@/views/events/show.vue'
-import TermsComponent from '@/views/legal/terms.vue'
-import PrivacyComponent from '@/views/legal/privacy.vue'
-import CookiesComponent from '@/views/legal/cookies.vue'
+// general pages
+import HomeView from '@/views/home/index.vue'
+import ExploreView from '@/views/explore/index.vue'
+// event pages
+import NewEventView from '@/views/events/new.vue'
+import EventView from '@/views/events/show.vue'
+// user pages
+import UserView from '@/views/users/index.vue'
+// legal pages
+import TermsView from '@/views/legal/terms.vue'
+import PrivacyView from '@/views/legal/privacy.vue'
+import CookiesView from '@/views/legal/cookies.vue'
+// error pages
 import EnableLocationComponent from '@/components/errors/LocationBlocked.vue'
 import NotFoundComponent from '@/components/errors/NotFound.vue'
 
@@ -21,60 +26,67 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: HomeComponent
+      component: HomeView
     },
     {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: DashboardComponent,
+      path: '/explore',
+      name: 'explore',
+      component: ExploreView,
       beforeEnter: (to, from, next) => {
         Guard.authCheck(to, from, next)
         // Guard.locationEnabled(to, from, next)
       }
     },
     {
-      path: '/events',
-      name: 'events',
-      component: EventsComponent,
+      path: '/events/new',
+      name: 'new-event',
+      component: NewEventView,
       beforeEnter: (to, from, next) => {
         Guard.authCheck(to, from, next)
         // Guard.locationEnabled(to, from, next)
-      },
-      children: [
-        {
-          path: 'new',
-          name: 'new-event',
-          component: NewEventComponent,
-          beforeEnter: (to, from, next) => {
-            Guard.authCheck(to, from, next)
-            // Guard.locationEnabled(to, from, next)
-          }
-        },
-        {
-          path: ':id',
-          name: 'show-event',
-          component: ShowEventComponent,
-          beforeEnter: (to, from, next) => {
-            Guard.authCheck(to, from, next)
-            // Guard.locationEnabled(to, from, next)
-          }
-        }
-      ]
+      }
+    },
+    {
+      path: '/events/:id',
+      name: 'show-event',
+      component: EventView,
+      beforeEnter: (to, from, next) => {
+        Guard.authCheck(to, from, next)
+        // Guard.locationEnabled(to, from, next)
+      }
+    },
+    {
+      path: '/users/:id',
+      name: 'user',
+      component: UserView,
+      beforeEnter: (to, from, next) => {
+        Guard.authCheck(to, from, next)
+        // Guard.locationEnabled(to, from, next)
+      }
+    },
+    {
+      path: '/users/:id/created',
+      name: 'user-created',
+      component: PrivacyView,
+      beforeEnter: (to, from, next) => {
+        Guard.authCheck(to, from, next)
+        // Guard.locationEnabled(to, from, next)
+      }
     },
     {
       path: '/terms',
       name: 'terms',
-      component: TermsComponent
+      component: TermsView
     },
     {
       path: '/privacy',
       name: 'privacy',
-      component: PrivacyComponent
+      component: PrivacyView
     },
     {
       path: '/cookies',
       name: 'cookies',
-      component: CookiesComponent
+      component: CookiesView
     },
     {
       path: '/enable-location',
